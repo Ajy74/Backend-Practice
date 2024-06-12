279c97fa12147b1caa9e302056cba88d05178817
Ajay Mourya <mouryaajay7463@gmai.com>
2024-06-12 14:51:59 +0530

    subscription model added

---
+++ src/models/subscription_model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // those who subscribe
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // for whome user is subscribe
+        ref: "User"
+    },
+}, {timestamps: true})
+
+export const Subscription = mongoose.model("subscription", subscriptionSchema); 

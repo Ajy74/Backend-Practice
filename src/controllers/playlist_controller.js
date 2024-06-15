3896a17b3728abe1d70aade7ea957d93d7e2f75b
Ajay Mourya <mouryaajay7463@gmai.com>
2024-06-15 15:18:27 +0530

    some playlist endpoint added

--- src/controllers/playlist_controller.js
+++ src/controllers/playlist_controller.js
@@ -7,8 +7,28 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
-
     //TODO: create playlist
+
+    if(
+        [name, description].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const newPlayList = await Playlist.create({
+        name,
+        description,
+        owner: mongoose.Types.ObjectId(req.user?._id)
+    });
+
+    if(!newPlayList){
+        throw new ApiError(500, "Something went wrong while creating playlist !")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, newPlayList, "Playlist created succefully !")
+    );
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
@@ -19,6 +39,43 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400, "Invalid playlist ID !")
+    }
+
+    const playlist = await Playlist.aggregate([
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId(playlistId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            fullname: 1,
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+    ]);
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {

403849fcea677f2d2eae9649e159eb7169004174
Ajay Mourya <mouryaajay7463@gmai.com>
2024-06-15 13:38:58 +0530

    bug resolved

--- src/routes/like_routes.js
+++ src/routes/like_routes.js
@@ -4,15 +4,15 @@ import {
     toggleCommentLike,
     toggleVideoLike,
     toggleTweetLike,
-} from "../controllers/like.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+} from "../controllers/like_controller.js"
+import {verifyJWT} from "../middlewares/auth_middleware.js"
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/toggle/v/:videoId").post(toggleVideoLike);
-router.route("/toggle/c/:commentId").post(toggleCommentLike);
-router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/toggle/video-like/:videoId").post(toggleVideoLike);
+router.route("/toggle/comment-like/:commentId").post(toggleCommentLike);
+router.route("/toggle/tweet-like/:tweetId").post(toggleTweetLike);
 router.route("/videos").get(getLikedVideos);
 
 export default router

54e9010d45ede931dcc83eafa9c4ca9f6494ec45
Ajay Mourya <mouryaajay7463@gmai.com>
2024-06-20 17:25:12 +0530

    subscription endpoint added

--- src/controllers/subscription_controller.js
+++ src/controllers/subscription_controller.js
@@ -9,16 +9,147 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    if(!mongoose.Types.ObjectId.isValid(channelId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    try {
+        const existingSubscription = await Subscription.findOne(
+            {
+                $channel: channelId,
+                $subscriber: req.user?._id
+            }
+        );
+
+        if(existingSubscription){
+            await Subscription.findByIdAndDelete(existingSubscription._id);
+            return res.status(200).json(new ApiResponse(200, {}, "subscription removed successfully!"));
+        }
+        else{
+            const newSubscribe = await Subscription.create({
+                subscriber: req.user?._id,
+                channel: channelId
+            });
+
+                
+            return res.status(200).json(new ApiResponse(200, newSubscribe , "Subcription added successfully!"));
+        }
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Something went wrong while toogle subscription !")
+    }
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(channelId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscriberList = await Subscription.aggregate([
+        {
+            $match: {
+                channel: channelId
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber",
+                pipeline: [
+                    {
+                        $project: {
+                            fullname: 1,
+                            username: 1,
+                            avatar: 1,
+                            coverImage: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: {
+                path: "subscriber"
+            }
+        },
+        {
+            $project: {
+                subscriber: 1
+            }
+        }
+    ]);
+
+    if(!subscriberList){
+        throw new ApiError(500, "Something went wrong fetching subscriber list !")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscriberList[0], "subscriber list fetched successfully !")
+    );
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(subscriberId)){
+        throw new ApiError(400, "Invalid Subscriber ID");
+    }
+
+    const channelList = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: subscriberId
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channel",
+                pipeline: [
+                    {
+                        $project: {
+                            fullname: 1,
+                            username: 1,
+                            avatar: 1,
+                            coverImage: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: {
+                path: "channel"
+            }
+        },
+        {
+            $project: {
+                channel: 1
+            }
+        }
+    ]);
+
+    if(!channelList){
+        throw new ApiError(500, "Something went wrong fetching channel list !")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channelList[0], "channel list fetched successfully !")
+    );
 })
 
 export {
--- src/routes/subscription_routes.js
+++ src/routes/subscription_routes.js
@@ -9,11 +9,9 @@ import {verifyJWT} from "../middlewares/auth_middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router
-    .route("/c/:channelId")
-    .get(getSubscribedChannels)
-    .post(toggleSubscription);
+router.route("/channnel/:channelId").get(getSubscribedChannels);
+router.route("/subsribe/:channelId").post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/user/:subscriberId").get(getUserChannelSubscribers);
 
 export default router
